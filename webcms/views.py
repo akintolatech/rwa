ce52a971f1c7e0b9f9fcd764a6e45b65acd463b3
akintolatech <akintolatechnologies@gmail.com>
2024-08-20 23:46:13 +0100

    website cms coding

--- webcms/views.py
+++ webcms/views.py
@@ -6,4 +6,11 @@ def index(request):
     context = {
         "msg": "Welcome to Django"
     }
-    return render (request, "webcms/index.html", context)
+    return render(request, "webcms/index.html", context)
+
+
+def menu(request):
+    context = {
+        "msg": "Menu Page"
+    }
+    return render(request, "webcms/menu.html", context)
